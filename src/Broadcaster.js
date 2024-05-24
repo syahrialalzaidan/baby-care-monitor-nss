bc1751636ab020584f7828e587b248b2a8df577b
Wentonn <nicolas.owen@gmail.com>
2024-05-24 14:02:29 +0700

    kurang dikit

--- src/Broadcaster.js
+++ src/Broadcaster.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 
-const Broadcaster = ({ socket }) => {
+const Broadcaster = ({socket}) => {
   const webcamRef = useRef(null);
 
   const sendFrame = async () => {
@@ -11,21 +11,13 @@ const Broadcaster = ({ socket }) => {
       socket &&
       socket.readyState === WebSocket.OPEN
     ) {
-      try {
-        const screenshot = webcamRef.current.getScreenshot();
-        if (screenshot && screenshot.startsWith("data:image")) {
-          const base64Frame = screenshot.split(",")[1]; // Remove the data:image/jpeg;base64, part
-          socket.send(JSON.stringify({ video: base64Frame }));
-          console.log("Full data URI:", screenshot);
-          console.log("Base64 data:", base64Frame);
-        } else {
-          console.error("Failed to capture screenshot: Invalid screenshot data");
-        }
-      } catch (error) {
-        console.error("Error capturing screenshot:", error);
+      const screenshot = webcamRef.current.getScreenshot();
+      if (screenshot) {
+        const base64Frame = screenshot.split(",")[1]; // Remove the data:image/jpeg;base64, part
+        socket.send(JSON.stringify({ video: base64Frame }));
+        console.log("Full data URI:", screenshot);
+        console.log("Base64 data:", base64Frame);
       }
-    } else {
-      console.error("Webcam or WebSocket not ready");
     }
   };
 
@@ -38,13 +30,11 @@ const Broadcaster = ({ socket }) => {
   }, [socket]);
 
   return (
-    <div style={{ visibility: 'hidden', position: 'absolute' }}>
+    <div>
       <Webcam
         audio={false}
         ref={webcamRef}
-        screenshotFormat="image/jpeg" // Try changing to "image/png" if needed
-        width={640}
-        height={480}
+        screenshotFormat="image/jpeg"
       />
     </div>
   );
