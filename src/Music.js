4d9dcda4e2e4de092e8aabfec1b427a46faa0ab4
Mochamad Syahrial Alzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2024-05-24 14:41:24 +0700

    feat: added close button

--- src/Music.js
+++ src/Music.js
@@ -1,9 +1,9 @@
 import Audio from "./AudioPlayer";
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import VideoClient from "./VideoClient"
+import VideoClient from "./VideoClient";
 import { WebSocketContext } from "./WebSocketContext";
-
+import { IoMdClose } from "react-icons/io";
 
 export default function Music() {
   const socket = useContext(WebSocketContext);
@@ -20,21 +20,25 @@ export default function Music() {
       </div>
 
       <div className="py-4 px-8 flex flex-col items-center justify-center">
-      <div className="py-4 px-8">
-        <div className="w-full flex justify-center">
+        <div className="py-4 px-8">
           <div className="w-full flex justify-center">
-            {socket && <VideoClient socket={socket} />}
+            <div className="w-full flex justify-center">
+              {socket && <VideoClient socket={socket} />}
+            </div>
           </div>
         </div>
-        </div>
 
-        <div className="bg-[#E4FFE0] mt-4 w-fit p-4 md:p-8 flex flex-col justify-center items-center gap-8 md:w-3/4">
+        <div className="bg-[#E4FFE0] mt-4 w-fit p-4 md:p-8 flex justify-center items-start gap-2 md:w-3/4">
           <div className="flex flex-wrap gap-8 justify-center items-center">
             <Audio title="Audio 1" link="baby-lullaby.mp3" />
             <Audio title="Audio 2" link="baby-lullaby.mp3" />
             <Audio title="Audio 3" link="baby-lullaby.mp3" />
             <Audio title="Audio 4" link="baby-lullaby.mp3" />
           </div>
+          <IoMdClose
+            className="text-6xl cursor-pointer"
+            onClick={handleStatus}
+          />
         </div>
       </div>
     </div>
